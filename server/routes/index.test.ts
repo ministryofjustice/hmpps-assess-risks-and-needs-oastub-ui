9b11cc3b4246f2e26f5bc4b53851b58c03ffc568
Denis Davidov <denislav.davidov@digital.justice.gov.uk>
2024-06-07 13:48:14 +0100

    fixing failing test

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -1,19 +1,12 @@
 import type { Express } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes, user } from './testutils/appSetup'
-import AuditService, { Page } from '../services/auditService'
-
-jest.mock('../services/auditService')
-
-const auditService = new AuditService(null) as jest.Mocked<AuditService>
 
 let app: Express
 
 beforeEach(() => {
   app = appWithAllRoutes({
-    services: {
-      auditService,
-    },
+    services: {},
     userSupplier: () => user,
   })
 })
@@ -24,17 +17,15 @@ afterEach(() => {
 
 describe('GET /', () => {
   it('should render index page', () => {
-    auditService.logPageView.mockResolvedValue(null)
-
     return request(app)
       .get('/')
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('This site is under construction...')
-        expect(auditService.logPageView).toHaveBeenCalledWith(Page.EXAMPLE_PAGE, {
-          who: user.username,
-          correlationId: expect.any(String),
-        })
+        expect(res.text).toContain('Given name')
+        expect(res.text).toContain('Family name')
+        expect(res.text).toContain('Gender')
+        expect(res.text).toContain('Assessment ID')
+        expect(res.text).toContain('Project')
       })
   })
 })
