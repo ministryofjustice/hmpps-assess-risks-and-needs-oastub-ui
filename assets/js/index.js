6c6bec390d9d1e32973fcc83918dc06983dd80f8
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-06-17 14:33:37 +0100

    Added ESBuild for building app and bundling/minifiying assets

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+import * as govukFrontend from 'govuk-frontend'
+import * as mojFrontend from '@ministryofjustice/frontend'
+import './copyPasteConfigurations'
+
+govukFrontend.initAll()
+mojFrontend.initAll()
