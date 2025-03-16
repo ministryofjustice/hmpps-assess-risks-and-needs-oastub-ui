6183fbb263da741509fd7caa7d8c3eef4e74d2b3
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-03-16 13:46:54 +0000

    Merge typescript-template#525

--- server/data/index.ts
+++ server/data/index.ts
@@ -3,6 +3,7 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
+import { AuthenticationClient, InMemoryTokenStore, RedisTokenStore } from '@ministryofjustice/hmpps-auth-clients'
 import { initialiseAppInsights, buildAppInsightsClient } from '../utils/azureAppInsights'
 import applicationInfoSupplier from '../applicationInfo'
 
@@ -10,28 +11,23 @@ const applicationInfo = applicationInfoSupplier()
 initialiseAppInsights()
 buildAppInsightsClient(applicationInfo)
 
-import HmppsAuthClient from './hmppsAuthClient'
 import { createRedisClient } from './redisClient'
-import RedisTokenStore from './tokenStore/redisTokenStore'
-import InMemoryTokenStore from './tokenStore/inMemoryTokenStore'
 import config from '../config'
 import HmppsAuditClient from './hmppsAuditClient'
+import logger from '../../logger'
 
 type RestClientBuilder<T> = (token: string) => T
 
-export const dataAccess = () => {
-  const hmppsAuthClient = new HmppsAuthClient(
+export const dataAccess = () => ({
+  applicationInfo,
+  hmppsAuthClient: new AuthenticationClient(
+    config.apis.hmppsAuth,
+    logger,
     config.redis.enabled ? new RedisTokenStore(createRedisClient()) : new InMemoryTokenStore(),
-  )
-  const hmppsAuditClient = new HmppsAuditClient(config.sqs.audit)
-
-  return {
-    applicationInfo,
-    hmppsAuthClient,
-    hmppsAuditClient,
-  }
-}
+  ),
+  hmppsAuditClient: new HmppsAuditClient(config.sqs.audit),
+})
 
 export type DataAccess = ReturnType<typeof dataAccess>
 
-export { HmppsAuthClient, RestClientBuilder, HmppsAuditClient }
+export { AuthenticationClient, RestClientBuilder, HmppsAuditClient }
